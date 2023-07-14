0a233b0813d532c839f7570177e7adb02c96eaa5
viviam_ribeiro <Viviam.Ribeiro@haw-hamburg.de>
2023-07-14 15:01:19 +0200

    Bug fix integrate syria plus turkey geo file

--- Model/Model/Location/LocationNodes/LocationNode.cs
+++ Model/Model/Location/LocationNodes/LocationNode.cs
@@ -16,19 +16,19 @@ public class LocationNode : IVectorFeature
 
 
 
-        if (VectorStructured.Data.ContainsKey("ADM3_EN") && !VectorStructured.Data.ContainsKey("ADM3_EN").ToString().EqualsIgnoreCase("null"))
+        if (VectorStructured.Data.ContainsKey("ADM3_EN") && !(VectorStructured.Data["ADM3_EN"] is null))
             name = VectorStructured.Data["ADM3_EN"].ToString();
-        else if (VectorStructured.Data.ContainsKey("ADM3_REF"))
+        else if (VectorStructured.Data.ContainsKey("ADM3_REF") && !(VectorStructured.Data["ADM3_REF"] is null))
             name = VectorStructured.Data["ADM3_REF"].ToString();
-        else if (VectorStructured.Data.ContainsKey("ADM3ALT1EN"))
+        else if (VectorStructured.Data.ContainsKey("ADM3ALT1EN") && !(VectorStructured.Data["ADM3ALT1EN"] is null))
             name = VectorStructured.Data["ADM3ALT1EN"].ToString();
-        else if (VectorStructured.Data.ContainsKey("ADM3ALT2EN"))
+        else if (VectorStructured.Data.ContainsKey("ADM3ALT2EN") && !(VectorStructured.Data["ADM3ALT2EN"] is null))
             name = VectorStructured.Data["ADM3ALT2EN"].ToString();
-        else if (VectorStructured.Data.ContainsKey("ADM2_EN"))
+        else if (VectorStructured.Data.ContainsKey("ADM2_EN") && !(VectorStructured.Data["ADM2_EN"] is null))
             name = VectorStructured.Data["ADM2_EN"].ToString();
-        else if (VectorStructured.Data.ContainsKey("adm2_en"))
+        else if (VectorStructured.Data.ContainsKey("adm2_en")&& !(VectorStructured.Data["adm2_en"] is null))
             name = VectorStructured.Data["adm2_en"].ToString();
-        else if (VectorStructured.Data.ContainsKey("adm1_en"))
+        else if (VectorStructured.Data.ContainsKey("adm1_en") && !(VectorStructured.Data["adm1_en"] is null))
             name = VectorStructured.Data["adm1_en"].ToString();
         
         VectorStructured.Data.Add("Name", name);
