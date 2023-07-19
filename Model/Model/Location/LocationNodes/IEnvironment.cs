4866baadde21709725fcbd722e0ab924c32b4b5a
viviam_ribeiro <Viviam.Ribeiro@haw-hamburg.de>
2023-07-20 00:22:22 +0200

    added interfaces (added new files to git)

---
+++ Model/Model/Location/LocationNodes/IEnvironment.cs
@@ -0,0 +1,24 @@
+﻿using Mars.Components.Environments;
+using Mars.Interfaces.Data;
+using Mars.Interfaces.Layers;
+using RefugeeSimulation.Model.Model.Shared;
+
+namespace LaserTagBox.Model.Model.Location.LocationNodes;
+
+public interface IEnvironment
+{
+    
+    /// <summary>
+    /// Returns the environment that contains refugees and locations
+    /// </summary>
+    /// <returns></returns>
+    public GeoHashEnvironment<AbstractEnvironmentObject> GetEnvironment();
+
+   /// <summary>
+    /// Returns a reference to the location that corresponds to the name paremeter.
+    /// If a location corresponding to the given name parameter isn't found, an ArgumentException will be thrown.
+    /// </summary>
+    /// <returns></returns>
+    public ILocation GetLocationByName(string locationName);
+   
+}
